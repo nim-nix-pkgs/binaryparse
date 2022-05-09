17aebc0ce025e415f4f6f157e97b1f9bc371bd44
hugosenari <hugosenari@gmail.com>
2022-05-09 20:35:35 -0300

    chore: re index binaryparse

--- flake.nix
+++ flake.nix
@@ -7,22 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."binaryparse-master".dir   = "master";
-  inputs."binaryparse-master".owner = "nim-nix-pkgs";
-  inputs."binaryparse-master".ref   = "master";
-  inputs."binaryparse-master".repo  = "binaryparse";
-  inputs."binaryparse-master".type  = "github";
-  inputs."binaryparse-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."binaryparse-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."binaryparse-v0_2_4".dir   = "v0_2_4";
-  inputs."binaryparse-v0_2_4".owner = "nim-nix-pkgs";
-  inputs."binaryparse-v0_2_4".ref   = "master";
-  inputs."binaryparse-v0_2_4".repo  = "binaryparse";
-  inputs."binaryparse-v0_2_4".type  = "github";
-  inputs."binaryparse-v0_2_4".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."binaryparse-v0_2_4".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
